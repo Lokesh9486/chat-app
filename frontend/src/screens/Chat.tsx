7be5783f945d211cb3cd6fc754941487b1754ccf
lokesh <lokesh.apr24@gmail.com>
2023-04-21 18:51:55 +0530

    try to add date

--- frontend/src/screens/Chat.tsx
+++ frontend/src/screens/Chat.tsx
@@ -1,23 +1,24 @@
 import { useGetChatDetailsQuery, useSendMessageMutation } from "../app/chatApi";
 import user from "../assets/images/user.png";
-import { useEffect, useRef, useState } from "react";
+import { FormEvent, Fragment, useEffect, useRef, useState } from "react";
 import { messageType, registerApiData, sidebarDataType } from "../types";
+import more from "../assets/images/more.png";
+
 const Chat = () => {
   const ulElement = useRef<HTMLInputElement | null>(null);
   const {
     data,
     // , isError, isFetching, isLoading, isSuccess
-  } =useGetChatDetailsQuery()
+  } = useGetChatDetailsQuery();
   // useGetChatDetailsQuery(
   //   "asdas", {
   //   pollingInterval: 3000,
   // }
   // );
   console.log(data);
-  
+
   const [sendMessage, { data: value, isError, isLoading, isSuccess }] =
     useSendMessageMutation();
-
 
   const sidebarData = data?.reduce(
     (
@@ -46,24 +47,33 @@ const Chat = () => {
     top: ulElement?.current?.scrollHeight,
     behavior: "smooth",
   });
-  
+
+  const recentDate=(data: messageType[]|undefined)=>{
+       console.log(data);
+      //  const uniqueDate=data?.filter(({createdAt},index)=>{
+      //   return data.indexOf(createdAt)===index
+      //  })
+  }
+
   useEffect(() => {
     if (currentChat === undefined) {
       setCurrentChat(sidebarData?.[0]?.id);
-      const chatData: any = data?.filter(
+      const chatData:  registerApiData[]|undefined = data?.filter(
         ({ _id }) => _id === sidebarData?.[0]?.id
       );
-      setCurrentChatData(chatData?.[0].message);
+      setCurrentChatData(chatData?.[0]?.message);
+      recentDate(chatData?.[0]?.message);
     } else {
-      const chatData: any = data?.filter(({ _id }) => _id === currentChat);
+      const chatData:  registerApiData[]|undefined = data?.filter(({ _id }) => _id === currentChat);
       setCurrentChatData(chatData?.[0].message);
+      recentDate(chatData?.[0]?.message);
     }
-  }, [data,userSendMessage]);
-
+    // currentChatData
+  }, [data, userSendMessage]);
 
   useEffect(() => {
     if (currentChat) {
-      const chatData: any = data?.filter(({ _id }) => _id === currentChat);
+      const chatData: registerApiData[]|undefined = data?.filter(({ _id }) => _id === currentChat);
       setCurrentChatData(chatData?.[0].message);
     }
   }, [currentChat]);
@@ -76,6 +86,11 @@ const Chat = () => {
   const shortTime = new Intl.DateTimeFormat("en", {
     timeStyle: "short",
   });
+
+  const formSubmitFunc=(e:FormEvent)=>{
+     e.preventDefault();
+     sendMessageFun();
+  }
 
   return (
     <div className="chat-page">
@@ -117,24 +132,38 @@ const Chat = () => {
         </div>
         <div className="chat-body" ref={ulElement}>
           {currentChatData?.map(({ type, message, createdAt }, index) => {
+            console.log(new Date(createdAt).toLocaleString("en-US", {
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            }));
+            
             return (
-              <>
-              <div
-                key={index}
-                className={
-                  type === "received" ? "other-chat-list" : "user-chat-list"
-                }
-              >
-                <img src={user} alt="user" className="profile-img-upload" />
-                <div className="message-con">
-                <p className="message">{message}</p>
-                 <p>{shortTime.format(new Date(createdAt))}</p>
+              <Fragment key={index}>
+                <div
+                  
+                  className={
+                    type === "received" ? "other-chat-list" : "user-chat-list"
+                  }
+                >
+                  <div className="chat-user-img-con">
+                  <button type="button" className="more-btn">
+                    <img src={more} alt="" />
+                  </button>
+                  <img src={user} alt="user" className="profile-img-upload" />
+                  </div>
+                  <div className="message-con">
+                    <p className="message">{message}</p>
+                    <p className="message-time">
+                      {shortTime.format(new Date(createdAt))}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              </>
+              </Fragment>
             );
           })}
-          <div className="user-message-conatiner">
+          <form onSubmit={formSubmitFunc} className="user-message-conatiner">
             <input
               type="text"
               className="message-input"
@@ -144,10 +173,10 @@ const Chat = () => {
                 setUserSendMaessage(e.target?.value)
               }
             />
-            <button type="submit" onClick={sendMessageFun}>
+            <button type="submit">
               Send
             </button>
-          </div>
+          </form>
         </div>
       </section>
     </div>
